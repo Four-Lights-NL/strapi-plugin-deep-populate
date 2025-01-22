307e374b20e5c6aaa4b60ec18786ba98cb5c52e0
Thomas Rijpstra <thomas@fourlights.nl>
2025-01-22 16:33:10 +0100

    chore: add playground

---
+++ playground/src/admin/app.tsx
@@ -0,0 +1,13 @@
+export default {
+  config: {
+    notifications: { releases: false },
+    translations: {
+      en: {
+        "Auth.form.welcome.title": "Deep-Populate playground",
+        "Auth.form.welcome.subtitle": "playground@example.com // Test1234",
+        "Auth.form.email.placeholder": "playground@example.com",
+      },
+    },
+  },
+  bootstrap(app) {},
+}
---
+++ playground/src/api/section/services/section.ts
@@ -0,0 +1,7 @@
+/**
+ * section services
+ */
+
+import { factories } from "@strapi/strapi"
+
+export default factories.createCoreService("api::section.section")
