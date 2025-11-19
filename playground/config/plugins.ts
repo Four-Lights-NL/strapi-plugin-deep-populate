ccbf28e84c5569e633b386f38f4107d0a704d8c4
Thomas Rijpstra <thomas@fourlights.nl>
2025-11-19 15:21:44 +0100

    chore(playground): add jwt secret for users-permissions plugin

--- playground/config/plugins.ts
+++ playground/config/plugins.ts
@@ -1,3 +1,12 @@
+import { randomBytes } from "node:crypto"
+
 export default ({ env }) => ({
   "deep-populate": { enabled: true },
+  "users-permissions": {
+    config: {
+      jwt: {
+        jwtSecret: env("STRAPI_JWT_SECRET") || randomBytes(16).toString("base64"),
+      },
+    },
+  },
 })
